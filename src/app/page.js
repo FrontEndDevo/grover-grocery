ed9d9e92147d4da85238f62ffd24967b8afd7d77
FrontEndDevo <mylinkacco@gmail.com>
2023-09-05 12:46:48 +0300

    Install FontAwesome package for icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <h1 className="font-bold text-red-500 font-Poppins text-9xl">Hello World</h1>;
+  return (
+    <h1 className="font-bold text-red-500 font-Poppins text-9xl">
+      Hello World
+    </h1>
+  );
 }

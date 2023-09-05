20c8b05b921fd1acc5aacb0438493f7f552f5efc
FrontEndDevo <mylinkacco@gmail.com>
2023-09-05 12:51:58 +0300

    Create Header component & add its images to image folder in public folder

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,9 @@
+import Header from "@/components/Header";
+
 export default function Home() {
   return (
-    <h1 className="font-bold text-red-500 font-Poppins text-9xl">
-      Hello World
-    </h1>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="text-4xl font-bold text-teal-800 font-Poppins">
+      Header
+    </header>
+  );
+};
+
+export default Header;

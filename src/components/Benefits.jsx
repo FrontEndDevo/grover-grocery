2e9411cdca212ba455bbb0499d0c888a0bd8d012
FrontEndDevo <mylinkacco@gmail.com>
2023-09-06 01:36:23 +0300

    Finish the Responsive Design for Benefits component

--- src/components/Benefits.jsx
+++ src/components/Benefits.jsx
@@ -25,17 +25,31 @@ const allGroverGroceryBenefits = [
 
 const Benefits = () => {
   const allBenefits = allGroverGroceryBenefits.map((item, i) => (
-    <div key={i}>
-      <FontAwesomeIcon icon={item.icon} />
-      <h3>{item.benefit}</h3>
-      <p>{item.description}</p>
+    <div
+      key={i}
+      className="relative px-2 py-12 text-center duration-1000 bg-white rounded-lg shadow-xl shadow-red-500/50 lg:shadow-red-50 hover:lg:shadow-red-500/100"
+    >
+      <FontAwesomeIcon
+        icon={item.icon}
+        className="mx-auto mb-20 text-red-500 w-28"
+      />
+      <h3 className="mb-4 text-2xl font-bold">{item.benefit}</h3>
+      <p className="px-16 text-sm text-gray-400">{item.description}</p>
     </div>
   ));
   return (
-    <section>
-      <h5>What we Serve</h5>
-      <h2>fruit and vegetable delivered to your home</h2>
-      <div>{allBenefits}</div>
+    <section className="py-20 bg-red-100 lg:py-40">
+      <div className="container">
+        <div className="font-bold text-center">
+          <h5 className="text-base text-red-400 lg:text-xl">What we Serve</h5>
+          <h2 className="mt-4 text-3xl leading-tight lg:text-5xl xl:px-60 lg:px-44 2xl:px-96 text-neutral-800">
+            fruit and vegetable delivered to your home
+          </h2>
+        </div>
+        <div className="grid grid-cols-1 gap-24 mx-4 mt-24 md:mx-0 md:grid-cols-2 lg:grid-cols-3">
+          {allBenefits}
+        </div>
+      </div>
     </section>
   );
 };

1c0d18946a4e28584816eef5ffabda0e7023a948
FrontEndDevo <mylinkacco@gmail.com>
2023-09-05 13:31:18 +0300

    Almost finish the JSX of Header component but still a separate component for the dynamic fruit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,48 @@
+import Image from "next/image";
+import header_chef_bg from "../../public/images/header/chef_bg.webp";
+import header_chef from "../../public/images/header/chef.webp";
+import temp_header from "../../public/images/header/temp_header_image_2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faClockRotateLeft, faPlay } from "@fortawesome/free-solid-svg-icons";
+
 const Header = () => {
-  return <header className="container py-8">hello world</header>;
+  return (
+    <header className="container py-8">
+      <div>
+        <Image src={header_chef_bg} alt="header chef background" />
+        <Image src={header_chef} alt="header chef background" />
+        <div>
+          <FontAwesomeIcon icon={faClockRotateLeft} />
+          <div>
+            <p>Delivary</p>
+            <span>30 Min</span>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button>
+          More than Faster
+          <Image src={temp_header} alt="More than Faster" />
+        </button>
+        <div>
+          <h2>
+            Groceries delivered in as little as <span>2 hours</span>
+          </h2>
+          <p>
+            Grocen atssures fresh grocery every morning to your family without
+            getting out in this pandemic.
+          </p>
+        </div>
+        <div>
+          <button>Order Now</button>
+          <button>
+            Order Process
+            <FontAwesomeIcon icon={faPlay} />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;

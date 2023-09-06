b8cd0e36ded0b9d5ef4a6bcccecf5cae3cb2d955
FrontEndDevo <mylinkacco@gmail.com>
2023-09-06 10:49:39 +0300

    Create WhyChooseUs component

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Benefits from "@/components/Benefits";
 import Header from "@/components/Header";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function Home() {
   return (
     <>
       <Header />
       <Benefits />
+      <WhyChooseUs />
     </>
   );
 }

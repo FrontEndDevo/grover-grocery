cbd9c128a136dbe968609a2931ad4c3a63d64433
FrontEndDevo <mylinkacco@gmail.com>
2023-09-07 18:13:57 +0300

    Finish the styles of Delivery component (Still Responsive)

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -2,18 +2,23 @@ import Image from "next/image";
 import deliveryImg from "../../public/images/delivery.webp";
 const Delivery = () => {
   return (
-    <section>
-      <div>
-        <Image src={deliveryImg} alt="Delivery man" />
+    <section className="container flex flex-col items-center justify-between lg:flex-row-reverse">
+      <div className="basis-4/12">
+        <Image src={deliveryImg} alt="Delivery man" className="w-full" />
       </div>
-      <div>
-        <h6>Home delivery</h6>
-        <h2>Sit at Home We Will Take Care Your Order</h2>
-        <p>
+
+      <div className="basis-5/12">
+        <h6 className="text-lg text-yellow-500 uppercase">Home delivery</h6>
+        <h2 className="mt-4 mb-8 text-5xl font-bold leading-tight text-neutral-800">
+          Sit at Home We Will Take Care Your Order
+        </h2>
+        <p className="text-lg text-gray-700">
           At vero eos et accusamus et iusto odio dignissimos ducimus qui
           blanditiis praesentium voluptatum deleniti atque corrupti quos
         </p>
-        <button>Explore Now</button>
+        <button className="w-full px-8 py-4 mt-12 text-xl text-red-400 duration-200 bg-transparent border-2 border-red-400 hover:text-white lg:w-fit rounded-xl hover:bg-red-400">
+          Explore Now
+        </button>
       </div>
     </section>
   );

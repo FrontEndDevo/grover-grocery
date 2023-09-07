631e93f869bc234ca37ae0849fd496755ca80c4f
FrontEndDevo <mylinkacco@gmail.com>
2023-09-07 17:54:06 +0300

    Finish the JSX of Delivery component

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+import deliveryImg from "../../public/images/delivery.webp";
 const Delivery = () => {
   return (
-    <section>Delivery</section>
-  )
-}
+    <section>
+      <div>
+        <Image src={deliveryImg} alt="Delivery man" />
+      </div>
+      <div>
+        <h6>Home delivery</h6>
+        <h2>Sit at Home We Will Take Care Your Order</h2>
+        <p>
+          At vero eos et accusamus et iusto odio dignissimos ducimus qui
+          blanditiis praesentium voluptatum deleniti atque corrupti quos
+        </p>
+        <button>Explore Now</button>
+      </div>
+    </section>
+  );
+};
 
-export default Delivery
+export default Delivery;

65aa90b39c966ad4d70faca6166e6bf1d0af699f
FrontEndDevo <mylinkacco@gmail.com>
2023-09-17 10:29:11 +0300

    Fetch the restaurants from firestore DB and store them into Redux store.

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
+"use client";
 import Benefits from "@/components/Benefits";
 import Delivery from "@/components/Delivery";
 import Header from "@/components/Header";
 import WhyChooseUs from "@/components/WhyChooseUs";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../config/firebase";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { restaurantsActions } from "@/slices/restaurantsSlice";
 
-export default function Home() {
+export default async function Home() {
+  const dispatch = useDispatch();
+  const restaurants = await getDocs(collection(db, "resturants"));
+  console.log(restaurants.docs[0].data());
+  restaurants.docs.forEach((rest) => {
+    dispatch(
+      restaurantsActions.addRestaurant({
+        restaurant: rest.data().restaurant,
+        foodItems: rest.data().foodItems,
+      })
+    );
+  });
+
   return (
     <>
       <Header />

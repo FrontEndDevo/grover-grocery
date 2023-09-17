594575f542dbc0b0363aef9f0284da55d7c2d023
FrontEndDevo <mylinkacco@gmail.com>
2023-09-17 10:23:18 +0300

    Create the Redux store and wrap the entire application with Redux Provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,10 @@
+"use client";
 import Footer from "@/components/Footer";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
-
+import { store } from "./store";
+import { Provider } from "react-redux";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -14,9 +16,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
-        <Footer />
+        <Provider store={store}>
+          <div>
+            <Navbar />
+            {children}
+            <Footer />
+          </div>
+        </Provider>
       </body>
     </html>
   );
---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import restaurantsSlice from "../slices/restaurantsSlice";
+
+export const store = configureStore({
+  reducer: {
+    restaurants: restaurantsSlice,
+  },
+});

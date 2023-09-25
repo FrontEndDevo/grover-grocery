357f924192e7cdd803b1a5dddcf377a628c048b8
FrontEndDevo <mylinkacco@gmail.com>
2023-09-25 20:27:48 +0300

    Finish the Third part of About section with styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import aboutTwo from "../../public/images/about/about_2.webp";
 import aboutThree from "../../public/images/about/about_3.webp";
+import aboutFour from "../../public/images/about/about_4.webp";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faLeaf,
@@ -9,6 +10,8 @@ import {
   faBagShopping,
   faTruckFast,
   faUserLock,
+  faCheck,
+  faArrowRight,
 } from "@fortawesome/free-solid-svg-icons";
 
 const ourServices = [
@@ -44,6 +47,12 @@ const ourServices = [
   },
 ];
 
+const deliveringFeatures = [
+  "Sed in metus pellentesque.",
+  "Fusce et ex commodo, aliquam nulla efficitur, tempus lorem.",
+  "Maecenas ut nunc fringilla erat varius.",
+];
+
 const About = () => {
   const aboutSercices = (
     <ul className="grid grid-cols-2 gap-4">
@@ -59,6 +68,20 @@ const About = () => {
             </h6>
             <p className="text-xs text-main-100">{srv.description}</p>
           </div>
+        </li>
+      ))}
+    </ul>
+  );
+
+  const ourDeliverFeatures = (
+    <ul className="mb-6">
+      {deliveringFeatures.map((feature) => (
+        <li className="flex items-center gap-2 my-2">
+          <FontAwesomeIcon
+            icon={faCheck}
+            className="w-6 p-2 text-3xl rounded-full bg-green-50 text-primary-700"
+          />
+          <p>{feature}</p>
         </li>
       ))}
     </ul>
@@ -95,12 +118,31 @@ const About = () => {
           <h2 className="mb-10 text-6xl font-bold text-main-700">
             100% Trusted Organic Food Store
           </h2>
-          <p className="text-base text-justify text-main-100">
+          <p className="mb-6 text-base text-justify text-main-100">
             Pellentesque a ante vulputate leo porttitor luctus sed eget eros.
             Nulla et rhoncus neque. Duis non diam eget est luctus tincidunt a a
             mi. Nulla eu eros consequat tortor tincidunt feugiat.
           </p>
           {aboutSercices}
+        </div>
+      </div>
+
+      <div className="container flex flex-row-reverse items-center justify-center gap-4">
+        <Image src={aboutFour} alt="We Delivered Part 3" />
+        <div>
+          <h2 className="mb-10 text-6xl font-bold text-main-700">
+            We Delivered, You Enjoy Your Order.
+          </h2>
+          <p className="mb-4 text-base text-justify text-main-100">
+            Ut suscipit egestas suscipit. Sed posuere pellentesque nunc,
+            ultrices consectetur velit dapibus eu. Mauris sollicitudin dignissim
+            diam, ac mattis eros accumsan rhoncus. Curabitur auctor bibendum
+            nunc eget elementum.
+          </p>
+          {ourDeliverFeatures}
+          <button className="flex items-center justify-center gap-4 px-8 py-4 text-white duration-200 bg-primary-500 rounded-xl hover:bg-primary-700">
+            Shop Now <FontAwesomeIcon icon={faArrowRight} />
+          </button>
         </div>
       </div>
     </section>

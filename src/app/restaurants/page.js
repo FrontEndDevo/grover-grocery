69b2ae92eb8b8ded6afb6a561d53c4291b91abf9
FrontEndDevo <mylinkacco@gmail.com>
2023-09-27 00:07:19 +0300

    Create Restaurants page and component

---
+++ src/app/restaurants/page.js
@@ -0,0 +1,11 @@
+import Restaurants from "@/components/Restaurants";
+
+const RestaurantsPage = () => {
+  return (
+    <>
+      <Restaurants />
+    </>
+  );
+};
+
+export default RestaurantsPage;

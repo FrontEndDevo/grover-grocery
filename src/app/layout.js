4316a6702a1c8a35ae0609ca1c87d03cc86e29c8
FrontEndDevo <mylinkacco@gmail.com>
2023-09-26 15:48:56 +0300

    Install slick-carousel to add react-slick styles to the application

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 "use client";
 import Footer from "@/components/Footer";
 import "./globals.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import { Inter } from "next/font/google";
 import Navbar from "@/components/Navbar";
 import { store } from "./store";
